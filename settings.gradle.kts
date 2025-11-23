8c6bb82f9710fcc09300ee967e52cc18ff481e91
GalateeM <galatee.m@gmail.com>
2025-11-23 14:06:13 +0100

    ajout settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,18 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
+        mavenCentral() // <- Maven Central suffit
     }
 }
 
 rootProject.name = "Vibing"
 include(":app")
- 
