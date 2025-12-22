b36531f41fa6f9008cb7e2fd20cebc9ef72a820f
GalateeM <galatee.m@gmail.com>
2025-12-22 18:18:32 +0100

    clean + recuperation dynamique couleurs

--- app/src/main/java/com/example/vibing/ui/score/PoiScoreFragment.java
+++ app/src/main/java/com/example/vibing/ui/score/PoiScoreFragment.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
